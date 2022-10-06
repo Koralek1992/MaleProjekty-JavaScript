28d01484a65a818e3f5fe40fc7b79ce145df8341
Koralek <dkoralek2@gmail.com>
2022-10-06 13:30:17 +0200

    projekt skończony

---
+++ Dodawanie elementów po kilknięciu - Samuraj/main.js
@@ -0,0 +1,16 @@
+const btn = document.querySelector('button')
+let number = 1
+
+const addElemet = function () {
+	const div = document.createElement('div')
+
+	if (number%5 == 0) {
+		div.classList.add('circle')
+	}
+
+	document.body.appendChild(div)
+	div.textContent = number
+	number++
+}
+
+btn.addEventListener('click', addElemet)
